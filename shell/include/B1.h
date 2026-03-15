cdf16dfb00873669a5967c788ce1ec09be7817b9
Bhavana-bit <sirnelli.bhavana@students.iiit.ac.in>
2026-03-15 22:29:06 +0530

    osnmp1

--- shell/include/B1.h
+++
@@ -1,9 +0,0 @@
-#ifndef B1_H
-#define B1_H
-
-// Implements the "hop" (cd) functionality
-// input: full command string from user
-// prev: stores the previous directory (for "hop -")
-void hop(char *input, char *prev);
-
-#endif
